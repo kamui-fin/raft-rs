1f37ef045df722d68960250d7dbbd00bad957038
Kamui <fin-kamui@pm.me>
2023-11-12 20:43:14 -0600

    feat: add log struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+// Barebones Raft implementation in Rust
+// Aim for high test coverage, and for easier testing initially:
+// - Develop without any networking (i.e. struct Server)
+// - In-memory state machine instead of persistent store
+
+enum NodeState {
+    Follower,
+    Candidate,
+    Leader,
+}
+
+enum RPCType {
+    AppendEntries,
+    RequestVote,
+}
+
+// e.g. SET x -> 1
+struct SetCommand {
+    key: String,
+    value: String,
+}
+
+struct LogItem {
+    command: SetCommand,
+    term: usize,
+}
+
+struct Log {
+    items: Vec<LogItem>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    unimplemented!()
 }
