4b1daab2e94c054411eaba6a4c661b500d668269
Kevin Ge <ge.kevin.a@gmail.com>
2023-11-13 19:10:32 -0600

    refactor: index -> indices

--- src/main.rs
+++ src/main.rs
@@ -98,9 +98,9 @@ struct ServerState {
 // Volatile state
 struct LeaderState {
     // for each server, index of the next log entry to send to that server (initialized to leader last log index + 1)
-    next_index: Vec<u64>,
+    next_indices: Vec<u64>,
     // for each server, index of highest log entry known to be replicated on server (initialized to 0, increases monotonically)
-    match_index: Vec<u64>,
+    match_indices: Vec<u64>,
 }
 
 trait Rpc<T> {
